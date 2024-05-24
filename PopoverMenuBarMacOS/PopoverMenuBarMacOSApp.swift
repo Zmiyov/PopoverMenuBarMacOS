bdc61f7aa033dd58387f042f2f4c6e5f26804d3f
Volodymyr <zmiyov88@gmail.com>
2024-05-24 12:18:17 +0300

    Initial Commit

---
+++ PopoverMenuBarMacOS/PopoverMenuBarMacOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  PopoverMenuBarMacOSApp.swift
+//  PopoverMenuBarMacOS
+//
+//  Created by Vladimir Pisarenko on 24.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct PopoverMenuBarMacOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
